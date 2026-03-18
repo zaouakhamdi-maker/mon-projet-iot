bc236cbc700f668af3658ee850da1b7f559e434b
zaouakhamdi-maker <zaouakhamdi@gmail.com>
2026-03-18 10:17:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,30 @@
-const CACHE_NAME = 'smart-home-v6';
-const ASSETS = ['./', './index.html', './manifest.json'];
+// Change la version (v8) pour forcer ton téléphone à se mettre à jour
+const CACHE_NAME = 'smart-home-v8';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js'
+];
 
-// Installation et mise en cache
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', (i) => {
+  i.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-// Nettoyage ancien cache
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
 });
 
-// Réception de l'ordre de notification
-self.addEventListener('push', (event) => {
-    const data = event.data ? event.data.json() : {};
-    const options = {
-        body: data.body || '🚨 Alerte : Mouvement détecté !',
-        icon: 'https://cdn-icons-png.flaticon.com/512/553/553376.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/553/553376.png',
-        vibrate: [500, 100, 500],
-        data: { url: './' },
-        actions: [{ action: 'open', title: 'Voir la maison' }]
-    };
-    event.waitUntil(self.registration.showNotification(data.title || 'ALERTE MAISON', options));
+self.addEventListener('fetch', (f) => {
+  f.respondWith(caches.match(f.request).then((res) => res || fetch(f.request)));
 });
 
-// Clic sur la notification
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    event.waitUntil(clients.openWindow('./'));
+self.addEventListener('push', (event) => {
+    const options = {
+        body: '🚨 Mouvement détecté dans la maison !',
+        icon: 'https://cdn-icons-png.flaticon.com/512/553/553376.png',
+        vibrate: [500, 100, 500]
+    };
+    event.waitUntil(self.registration.showNotification('ALERTE INTRUSION', options));
 });
