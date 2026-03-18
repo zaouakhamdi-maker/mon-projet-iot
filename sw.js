b9f040b1822fd23c7061637f8009101683415ff3
zaouakhamdi-maker <zaouakhamdi@gmail.com>
2026-03-18 11:03:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
-const CACHE_NAME = 'smart-home-v1'; 
-const ASSETS = [ './', './index.html', './manifest.json', 'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js', 'https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js' ]; 
-self.addEventListener('install', (i) => { i.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); }); 
-self.addEventListener('fetch', (f) => { f.respondWith(caches.match(f.request).then((res) => res || fetch(f.request))); }); 
-self.addEventListener('push', (event) => { const options = { body: '🚨 Mouvement détecté dans la maison !', icon: 'https://cdn-icons-png.flaticon.com/512/553/553376.png', vibrate: [500, 100, 500] }; 
-                                          event.waitUntil(self.registration.showNotification('ALERTE INTRUSION', options)); }); 
+const CACHE_NAME = 'smart-home-v13.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
+
+self.addEventListener('push', (event) => {
+    const options = {
+        body: '🚨 Mouvement détecté !',
+        icon: 'https://cdn-icons-png.flaticon.com/512/553/553376.png',
+        vibrate: [500, 100, 500],
+        tag: 'alert'
+    };
+    event.waitUntil(self.registration.showNotification('SMART HOME', options));
+});
